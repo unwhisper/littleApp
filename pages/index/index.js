390cd5f70b36523039946b80e598f0f9990d69a5
leijie <591856607@qq.com>
2018-09-05 09:22:53 +0800

    first

---
+++ pages/index/index.js
@@ -0,0 +1,30 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    news:{}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    
+  },
+  onLoad: function () {
+    var that =this
+    wx.request({
+      //上线接口地址要是https测试可以使用http接口方式
+      url: 'https://www.apiopen.top/satinApi?type=1&page=1',
+      data: {},
+      method: 'GET',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        that.setData({ news: res.data.data });
+        console.log(res.data.data);
+      }
+
+    })
+  }
+})
