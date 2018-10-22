ebdce5e25f9e1cb0d22411f38820e383fdb00a06
15095868104 <591856607@qq.com>
2018-10-22 17:19:45 +0800

    add

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,7 +14,7 @@ Page({
     var that =this
     wx.request({
       //上线接口地址要是https测试可以使用http接口方式
-      url: 'https://www.apiopen.top/satinApi?type=1&page=1',
+      url: 'https://www.apiopen.top/satinGodApi?type=4&page=9',
       data: {},
       method: 'GET',
       header: {
--- pages/video/video.js
+++ pages/video/video.js
@@ -1,46 +1,33 @@
 // pages/video/video.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    video:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this
+    wx.request({
+      //上线接口地址要是https测试可以使用http接口方式
+      url: 'https://www.apiopen.top/satinGodApi?type=5&page=2',
+      data: {},
+      method: 'GET',
+      header: {
+        'content-type': 'application/json'
+      },
+      success: function (res) {
+        that.setData({ video: res.data.data });
+        console.log(res.data.data);
+      }
 
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+    })
   },
 
   /**
